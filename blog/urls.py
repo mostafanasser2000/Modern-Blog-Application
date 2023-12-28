5f41e8c23bba94a320fe94dd062c6b7e97f4356b
mostafanasser2000 <mustafanasser503@gmail.com>
2023-12-28 20:52:10 +0200

    change some url end points

--- blog/urls.py
+++ blog/urls.py
@@ -5,15 +5,15 @@ app_name = "blog"
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path("posts/", views.PostList.as_view(), name="post_list"),
-    path("posts/<int:pk>/", views.PostDetail.as_view(), name="post_detail"),
+    path("posts/<int:pk>/<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
     path(
         "posts/author/<str:author_name>/",
         views.PostList.as_view(),
         name="posts_by_author",
     ),
     path("posts/create/", views.PostCreate.as_view(), name="post_create"),
-    path("posts/update/<int:pk>/", views.PostUpdate.as_view(), name="post_update"),
-    path("posts/delete/<int:pk>/", views.PostDelete.as_view(), name="post_delete"),
+    path("posts/<int:pk>/update/", views.PostUpdate.as_view(), name="post_update"),
+    path("posts/<int:pk>/delete/", views.PostDelete.as_view(), name="post_delete"),
     path("posts/<int:post_id>/share/", views.post_share, name="post_share"),
     path("posts/<int:post_id>/comment/", views.post_comment, name="post_comment"),
     path("tags/<slug:tag>/", views.PostList.as_view(), name="posts_by_tag"),
